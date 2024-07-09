7f58bfc76cf2b14134fb88aa1ad5e31a51742488
Brice <b.libert@ideematic.com>
2024-07-09 13:54:28 +0200

    fix bug

--- guess.rb
+++ guess.rb
@@ -2,6 +2,7 @@
 
 require 'colorize'
 
+# starts the game and generates clues based on the guess
 class Guess
   def initialize
     @previous_guesses = []
@@ -9,35 +10,40 @@ class Guess
     @highest_points = 0
     @no_output_numbers = []
     @code = make_code
-    @output = []
     @round = 12
     state_rules
     start_game
   end
 
   def state_rules
-    p 'Guess the code and in the tight order. The numbers will be 1-4'
-    p "'!' means a number is in the right place, don't change it"
-    p "'?' means a number is in the code, but not in the right place, change the location"
+    puts 'Guess the code and in the right order. The numbers will be  1-4'
+    puts "'!' means a number is in the right place, don't change it"
+    puts "'?' means a number is not in the right place, change the location"
   end
 
+  # game logic
   def start_game
     while @round.positive?
       @guess = input_guess
       @output = make_output
       update_best_guess
       display_info
+      @round -= 1
       break if solved?
     end
     finish_game
   end
 
+  # MAKE_OUTPUT
+
+  # generate the clues that coincide with guesses
   def make_output
     @output = correct_location
     @output.push(correct_number)
     @output.flatten
   end
 
+  # ! is added for each num in the right spot
   def correct_location
     output = []
     @code.each_with_index do |num, index|
@@ -46,20 +52,23 @@ class Guess
     output
   end
 
+  # ? is added for each num in the guess but not in the right spot
   def correct_number
     saved_numbers = save_list
     output = []
     index = 0
     @code.length.times do
       if @guess.include?(@code[index]) && !saved_numbers.include?(@code[index])
-        output.push('?')
         saved_numbers.push(@code[index])
+        output.push('?')
       end
+
       index += 1
     end
     output
   end
 
+  # make a list of the numbers that are in the right location. Don't add a ? for these
   def save_list
     saved = []
     @code.each_with_index do |num, index|
@@ -68,6 +77,9 @@ class Guess
     saved
   end
 
+  # UPDATE_BEST_GUESS
+
+  # update best_attempt and previous_guesses
   def update_best_guess
     if @previous_guesses.empty?
       @best_guess = @guess
@@ -82,6 +94,7 @@ class Guess
     compare_attempts(points)
   end
 
+  # this determines which attempt was better
   def calculate_points
     points = 0
     unless @output.empty?
@@ -93,12 +106,15 @@ class Guess
     points
   end
 
+  # using the points reassign best attempts
   def compare_attempts(current)
     return unless current > @highest_points
 
     @best_guess = @guess
     @highest_points = current
   end
+
+  # SOLVED?
 
   def solved?
     return unless @code == @guess
